80ce43e602a0f5a2ba6021568130c826c04e76f6
Taj <tjgurwara99@gmail.com>
2022-01-29 17:59:52 +0000

    feat: ast interfaces and parser scaffold

---
+++ pkg/ast/logic.go
@@ -0,0 +1,40 @@
+package ast
+
+import "github.com/tjgurwara99/monkey-go/pkg/token"
+
+type Program struct {
+	Statements []Statement
+}
+
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+	return ""
+}
+
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+var _ Statement = (*LetStatement)(nil) // this is a compile time interface compliance check
+
+func (l *LetStatement) statementNode() {}
+
+func (l *LetStatement) TokenLiteral() string {
+	return l.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token
+}
+
+var _ Expression = (*Identifier)(nil) // compile time interface compliance check same as above
+
+func (i *Identifier) expressionNode() {}
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
